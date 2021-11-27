fe3e3e67345e6d1feecf244be8da84a28a1d6f97
Thomas <thomas.sembely@gmail.com>
2021-11-27 13:10:44 +0100

    getdate added & header building header in progress

--- srcs/HandleRequest.cpp
+++ srcs/HandleRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: thsembel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 16:49:19 by thsembel          #+#    #+#             */
-/*   Updated: 2021/11/27 12:43:08 by thsembel         ###   ########.fr       */
+/*   Updated: 2021/11/27 13:10:24 by thsembel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Client.hpp"
@@ -14,16 +14,16 @@
 
 void	buildHeader(Response &response)
 {
-	response.headers = response.version + ' ' +  response.status_code + "\n";
-	response.headers += "Content-Length: " + std::to_string(response.body_len) + "\n"; 
-	response.headers += "Content-Type: " + response.content_type + "\n";
-	response.headers += "Date: " + ft_getDate() + "\n";
-	response.headers += "Last-Modified: ";
-	response.headers += "Location: " + response.location + "\n";
+	response.headers = response.version + ' ' +  response.status_code + "\r\n";
+	response.headers += "Content-Length: " + std::to_string(response.body_len) + "\r\n"; 
+	response.headers += "Content-Type: " + response.content_type + "\r\n";
+	response.headers += "Date: " + ft_getDate() + "\r\n";
+	response.headers += "Last-Modified: \r\n";
+	response.headers += "Location: " + response.location + "\r\n";
 	if (response.name != "")
-		response.headers += "Server: " + response.name + "\n";
+		response.headers += "Server: " + response.name + "\r\n";
 	else
-		response.headers += "Server: Webserv\n";
+		response.headers += "Server: Webserv\r\n";
 }
 
 
@@ -32,18 +32,27 @@ void	HandleGET(Client &client)
 {
 	if (client.request.config.methods.find("GET") == std::string::npos)
 		client.response.status_code = NOTALLOWED;
+	else
+		client.response.status_code = OK;
+	
+
+	
 }
 
 void	HandlePOST(Client &client)
 {
 	if (client.request.config.methods.find("POST") == std::string::npos)
 		client.response.status_code = NOTALLOWED;
+	else
+		client.response.status_code = OK;
 }
 
 void	HandleDELETE(Client &client)
 {
 	if (client.request.config.methods.find("DELETE") == std::string::npos)
 		client.response.status_code = NOTALLOWED;
+	else
+		client.response.status_code = OK;
 }
 
 void	HandleBAD(Client &client)
